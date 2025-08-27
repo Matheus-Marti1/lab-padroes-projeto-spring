0c4adf608e76eaa9846654d0635ac7affd9ef3fa
Matheus Marti <30555147+Matheus-Marti1@users.noreply.github.com>
2025-08-27 16:45:14 -0300

    implementar o padrão de projeto Builder na classe Cliente

--- src/main/java/one/digitalinnovation/gof/model/Cliente.java
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -5,16 +5,46 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Setter
 @Getter
+@Setter
 @Entity
 public class Cliente {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String nome;
-	@ManyToOne
-	private Endereco endereco;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
 
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    private Endereco endereco;
+
+    protected Cliente() {}
+
+    public static ClienteBuilder builder() {
+        return new ClienteBuilder();
+    }
+
+    public static class ClienteBuilder {
+        private String nome;
+        private Endereco endereco;
+
+        private ClienteBuilder() {}
+
+        public ClienteBuilder nome(String nome) {
+            this.nome = nome;
+            return this;
+        }
+
+        public ClienteBuilder endereco(Endereco endereco) {
+            this.endereco = endereco;
+            return this;
+        }
+
+        public Cliente build() {
+            Cliente cliente = new Cliente();
+            cliente.setNome(this.nome);
+            cliente.setEndereco(this.endereco);
+            return cliente;
+        }
+    }
 }
